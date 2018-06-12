fce309ae6c0b1ee1d1d5299fe908eac82a004916
Unknown <peter.delvaux@diligentia-uitgeverij.be>
2018-06-12 12:00:07 +0200

    stupid me forgot to build the latest changes
    stupid... I know...

--- dist/types/GUI/PageGuideGUI.d.ts
+++ dist/types/GUI/PageGuideGUI.d.ts
@@ -1,16 +1,18 @@
 import { IButtonDefinition } from '../interfaces/IButtonDefinition';
+import { PageGuideItem } from '../PageGuideItem';
 export declare class PageGuideGui {
     private buttonDefinition;
     private canvas;
     private ctx;
     private btnStart;
-    private targets;
+    private target;
     constructor(buttonDefinition?: boolean | IButtonDefinition);
     onStart(callback: (this: HTMLElement, ev: MouseEvent) => any): void;
     onEnd(callback: (this: HTMLElement, ev: MouseEvent) => any): void;
     start(): void;
     update(): void;
+    private roundRect(ctx, x, y, width, height, radius, fill, stroke);
     stop(): void;
-    highlight(elements: NodeListOf<HTMLElement>): void;
+    highlight(target: PageGuideItem): void;
     private renderBackdrop();
 }
